197a8ac3416f3ca80ab2cb01234831e6bf708a33
moi <125820548+nojia1775@users.noreply.github.com>
2024-09-16 07:41:56 +0000

    j'ai juste enleve des printf

--- parsing/pipe.c
+++ parsing/pipe.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 14:11:59 by nadjemia          #+#    #+#             */
-/*   Updated: 2024/09/16 07:33:04 by codespace        ###   ########.fr       */
+/*   Updated: 2024/09/16 07:41:35 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,15 @@ int	conform_pipe(char *str)
 		if (ft_isalnum(str[i]))
 			charac = 1;
 		if (str[i] == '|' && charac == 0 && !in_single && !in_double)
-			return (printf("1\n"), 0);
+			return (0);
 		else if (str[i] == '|' && str[i + 1] == '|' && !in_double
 			&& !in_single)
-			return (printf("2\n"), 0);
+			return (0);
 		else if (str[i] == '|' && !in_double && !in_single)
 			charac = 0;
 	}
 	if (!charac)
-		return (printf("3\n"), 0);
+		return (0);
 	return (1);
 }
 
