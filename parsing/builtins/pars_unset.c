6f82f83d4016308109bf6b38267af8fc0cecb02b
noah <nojia.adjemrose@gmail.com>
2024-09-26 16:34:52 +0200

    unset fixed

--- parsing/builtins/pars_unset.c
+++ parsing/builtins/pars_unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pars_unset.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: noah <noah@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: nadjemia <nadjemia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 02:08:02 by almichel          #+#    #+#             */
-/*   Updated: 2024/09/14 23:15:42 by noah             ###   ########.fr       */
+/*   Updated: 2024/09/26 16:34:33 by nadjemia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,7 @@ void	pars_unset(t_token *cur, t_global *global)
 	unset = cur->cmd_pipex;
 	g_sigint_received = 0;
 	if (!unset[1])
-	{
-		free_double_tabs(unset);
 		return ;
-	}
 	else
 	{
 		while (unset[i])
